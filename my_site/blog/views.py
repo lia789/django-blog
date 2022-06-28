a0e52af3e912382aeaa304388f4bdd2dd6d38573
lia789 <liahabib53@gmail.com>
2022-06-28 17:51:35 +0600

    #1 template inheritance code added

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 
 def starting_page(request):
-    pass
+    return render(request, "blog/home_page.html")
 
 
 
 def posts(request):
-    pass
+    return render(request, "blog/post_list.html")
 
 
 
 def post_detail(request):
-    pass
+    return render(request, "blog/post_detail.html")
 
 
